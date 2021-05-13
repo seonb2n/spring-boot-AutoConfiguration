aad5c34bb13fa03fb9c0920c09b38913349452ee
seonb2n <626ksb@gmail.com>
2021-05-13 16:07:15 +0900

    tomcat 서버를 code 로 만들어봤다.

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -1,19 +1,54 @@
 package Application;
 
 import me.seonb2n.Holoman;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.HttpServerErrorException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 
-@SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+    public static void main(String[] args) throws LifecycleException, IOException {
+        Tomcat tomcat = new Tomcat();
+
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        String docBase = Files.createTempDirectory("tomcat-basedir").toString();
+
+        Context context = tomcat.addContext("/", docBase);
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("<html><head><title>");
+                writer.println("Hey, Tomcat");
+                writer.println("</title></head");
+                writer.println("<body><h1>Hello Tomcat</h1></body>");
+                writer.println("</html>");
+            }
+        };
+
+        String servletName = "Hello Servlet";
+        tomcat.addServlet("/", servletName, servlet);
+        context.addServletMappingDecoded("/hello",servletName);
+
+        tomcat.start();
+
     }
 }
