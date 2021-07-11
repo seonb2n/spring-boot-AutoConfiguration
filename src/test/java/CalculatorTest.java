baabfef6dfc6b82be5ffb8f102530c3ab97840f5
seonb2n <626ksb@gmail.com>
2021-07-11 11:12:14 +0900

    Junit 활용 테스트

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,54 @@
+import Application.component.Calculator;
+import Application.component.DollarCalculator;
+import Application.component.MarketApi;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.yaml.snakeyaml.error.Mark;
+
+@ExtendWith(MockitoExtension.class)
+public class CalculatorTest {
+
+    @Mock
+    public MarketApi marketApi;
+
+    @BeforeEach
+    public void init() {
+        //marketApi 의 connect 가 일어날 때, return 값을 임의로 조정
+        Mockito.lenient().when(marketApi.connect()).thenReturn(3000);
+    }
+
+    @Test
+    public void testHello() {
+        System.out.println("hello");
+    }
+
+    @Test
+    public void dollarTest() {
+        MarketApi marketApi = new MarketApi();
+        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+        dollarCalculator.init();
+
+        Calculator calculator = new Calculator(dollarCalculator);
+
+        Assertions.assertEquals(22000, calculator.sum(10, 10));
+        Assertions.assertEquals(0, calculator.minus(10, 10));
+    }
+
+    @Test
+    public void mockTest() {
+        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+        dollarCalculator.init();
+
+        Calculator calculator = new Calculator(dollarCalculator);
+
+        Assertions.assertEquals(60000, calculator.sum(10, 10));
+        Assertions.assertEquals(0, calculator.minus(10, 10));
+    }
+
+
+}
